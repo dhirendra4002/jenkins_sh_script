e8c93a7087289a8c95513afd9c3c7735cf3e4049
dhirendra4002 <86907513+dhirendra4002@users.noreply.github.com>
2022-10-15 11:49:06 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
+sudo apt update
 sudo apt install default-jdk -y
 sudo mkdir -p /usr/share/keyrings
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
